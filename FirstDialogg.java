705687e02c97f65a661ecd5ce6ef0888da759e91
nqc992 <cuongblueko123@gmail.com>
2025-02-28 14:52:14 +0700

    Add files via upload

---
+++ FirstDialogg.java
@@ -0,0 +1,11 @@
+package test;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialogg {  // Ensure this matches the filename
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
