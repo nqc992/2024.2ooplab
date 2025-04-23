84a4b784397bd9e2e70c4cc30d7feba12bed1106
nqc992 <cuongblueko123@gmail.com>
2025-04-23 22:35:54 +0700

    Add files via upload

---
+++ Lab 03/AIMSproject/src/hust/soict/hedspi/aims/media/Book.java
@@ -0,0 +1,29 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book extends Media {
+    private List<String> authors = new ArrayList<>();
+
+    public Book(int id, String title, String category, float cost) {
+        super(id, title, category, cost);
+    }
+
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Book - " + super.toString() + " - Authors: " + authors;
+    }
+}
