d2142f6349b3990f02767a5311dcfc93e69e8c2f
nqc992 <cuongblueko123@gmail.com>
2025-05-24 21:56:58 +0700

    Delete Lab 05 directory

--- Lab 05/AIMSProject/src/hust/soict/hedspi/aims/media/Playable.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.hedspi.aims.media;
-
-import hust.soict.hedspi.aims.exception.PlayerException; // Import PlayerException
-
-public interface Playable {
-    // Chữ ký phương thức đã được cập nhật: giờ đây khai báo rằng nó có thể ném PlayerException
-    void play() throws PlayerException;
-}
